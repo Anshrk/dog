d7b1d39c5e9c8a010841b6b7fb6f740d26161b7d
Anshrk <anshrk.13@gmail.com>
2021-07-08 17:26:50 +0530

    some new change ig

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
-
 fn main() {
+    // set of all command line arguments
     let args: Vec<String> = env::args().collect();
-
+    // looping through all commands
     for argument in args.iter(){
+        // defautl argument is passed as the file location
         if argument==&args[0] {
             continue;
         }
-
+        // open the file in the argument
         let mut file = File::open(&argument.as_str())
         .expect("Cannot Open the file");
-
-        
         let mut contents = String::new();
+        // add the contents of the file to a string
         file.read_to_string(&mut contents)
         .expect("Error reading the file");
-
+        // print the content of the file
         println!("{}", contents);
     }
 }
